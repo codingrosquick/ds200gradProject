63d120b93c0c2c39e239f7cf5e24372914fb68e9
Noé Carras <noecarras@MacBook-Pro-de-Noe.local>
2022-04-15 15:44:37 -0700

    added loading

--- raw/gdrive_refs.py
+++ raw/gdrive_refs.py
@@ -1,6 +1,6 @@
 gdrive_dic = [
     ["1Y2oC3I6LFos0d755rqWnygHBcO2sYHZS", "annual_aqi__by_county.csv"],
-    ["1Y2oC3I6LFos0d755rqWnygHBcO2sYHZS", "daily_aqi__by_county.csv"]
+    ["1Y2oC3I6LFos0d755rqWnygHBcO2sYHZS", "daily_aqi__by_county.csv"],
     ["15cPDL3T8bL9E-jhlzmohgb20AtazMunV", "Air_Quality_Measures_on_the_National_Environmental_Health_Tracking_Network.csv"],
     ["18ImVWH03BPfSdt4LXyMEDbfcf5C0LmIo", "data-who-attributable-death-rate-air-pollutants-other-version.csv"],
     ["1TDj5BHrmRN-eGtBzzif-qdcG_QIRjmE8", "data-who-attributable-death-rate-air-pollutants.csv"],
